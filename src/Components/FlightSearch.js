4ca205d81cf1be78c73eae0ec5cc4048b6db6ced
Zankar1992 <zankar.logicalstreet@gmail.com>
2022-02-03 17:46:30 +0530

    changes data

--- src/Components/FlightSearch.js
+++ src/Components/FlightSearch.js
@@ -5,48 +5,47 @@ class FlightSearch extends Component {
 
   constructor(props) {
     super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-
-    this.state = {
+    this.state = [{
       origin: '',
       destination: '',
-    };
+    }];
+    this.onChangeOrigin = this.onChangeOrigin.bind(this);
+    this.onChangeDestination = this.onChangeDestination.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  onChangeOrigin(e) {
+    this.setState({ origin: e.target.value })
   }
 
-  // onChangeDestination(e) {
-  //   this.setState({ destination: e.target.value })
-  // }
+  onChangeDestination(e) {
+    this.setState({ destination: e.target.value })
+  }
 
-  handleFormSubmit(e) {
+  onSubmit(e) {
     e.preventDefault()
-    localStorage.setItem('user',JSON.stringify(this.state));
-    // this.setState({
-    //   origin: this.userdata.origin,
-    //   destination: this.userdata.destination,
-    // })
+
+    this.setState({
+      origin: '',
+      destination: '',
+    })
   }
 
-  componentDidMount() {
-    this.userdata = JSON.parse(localStorage.getItem('user'));
+  // componentDidMount() {
+  //   this.userdata = JSON.parse(localStorage.getItem('user'));
 
-    if (localStorage.getItem('user')) {
-      this.setState({
-        origin: this.userdata.origin,
-        destination: this.userdata.destination
-      })
-    } else {
-      this.setState({
-        origin: '',
-        destination: ''
-      })
-    }
-  }
+  //   if (localStorage.getItem('user')) {
+  //     this.setState({
+  //       origin: this.userdata.origin,
+  //       destination: this.userdata.destination
+  //     })
+  //   } else {
+  //     this.setState({
+  //       origin: '',
+  //       destination: ''
+  //     })
+  //   }
+  // }
 
   // componentWillUpdate(nextProps, nextState) {
   //   localStorage.setItem('user', JSON.stringify(nextState));
@@ -55,16 +54,16 @@ class FlightSearch extends Component {
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Origin:</label>
-            <input type="text" className="form-control" value={this.state.origin} onChange={this.handleChange} />
-          </div> <br />
+            <input type="text" className="form-control" value={this.state.origin} onChange={this.onChangeOrigin} />
+          </div> <br/>
           <div className="form-group">
             <label>Destination:</label>
-            <input type="text" className="form-control" value={this.state.destination} onChange={this.handleChange} />
+            <input type="text" className="form-control" value={this.state.destination} onChange={this.onChangeDestination} />
           </div>
-          <br />
+          <br/>
           <button type="submit" className="btn btn-primary btn-block">Add</button>
         </form >
       </div>
